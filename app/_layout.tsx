078e373335b8843098a12a44d327191f424d79a1
Ali Harakeh <34475779+aliharakeh@users.noreply.github.com>
2025-03-30 03:05:44 +0300

    Update app configuration and assets

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,7 @@
 import { DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
     return (
