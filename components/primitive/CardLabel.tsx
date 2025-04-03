804223217df4a3faab0e7579b53393d14de84268
Ali Harakeh <34475779+aliharakeh@users.noreply.github.com>
2025-04-03 13:21:12 +0300

    add i18n

--- components/primitive/CardLabel.tsx
+++ components/primitive/CardLabel.tsx
@@ -3,16 +3,20 @@ import { Text, View } from 'react-native';
 export interface CardLabelProps {
     label: string;
     value: string | string[];
+    rtl?: boolean;
 }
 
 export function CardLabel(p: CardLabelProps) {
     const value = Array.isArray(p.value) ? p.value : [p.value];
     return (
-        <View className="flex-row gap-2 w-full">
-            <Text className="font-semibold w-[120px]">{p.label}</Text>
-            <View>
+        <View className={`flex-row gap-2 w-full ${p.rtl ? 'flex-row-reverse' : ''}`}>
+            <Text className={`font-semibold w-[120px] ${p.rtl ? 'text-right' : ''}`}>
+                {p.label}
+            </Text>
+
+            <View className={p.rtl ? 'flex-1 items-end' : 'flex-1'}>
                 {value.map((v, i) => (
-                    <Text key={i} className="text-gray-500">
+                    <Text key={i} className={`text-gray-500 ${p.rtl ? 'text-right' : ''}`}>
                         {v}
                     </Text>
                 ))}
